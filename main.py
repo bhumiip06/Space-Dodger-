81d87b800780ca3b40a112d8a41f00abbf6e7829
bhumiip06 <bhumikapimoli87@gmail.com>
2026-08-02 00:40:36 +0530

    feat: add start menu, background image, dynamic difficulty, and improved game over screen

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ player_height=64
 player=pygame.transform.scale(player,(player_width,player_height))
 
 #   player coordinates (In python coordinates starts from top left )
-player_x=100   # increases to right
-player_y=100   #increases downwards
+player_x=(width-player_width)//2   # increases to right
+player_y=(height-player_height)//2   #increases downwards
 
 speed=3 #   controls how much player moves each game-loop iteration
 
@@ -61,6 +61,7 @@ white=255,255,255
 running=True
 
 game_over=False
+show_menu=True
 font = pygame.font.Font(None,60)
 
 #get highscore
@@ -81,7 +82,16 @@ highscore=load_highscore()
 score_font=pygame.font.Font(None,36)
 restart_font=pygame.font.Font(None,32)
 
-restart_button=pygame.Rect(width//2-75,height//2+50,150,50)
+restart_button=pygame.Rect(width//2-75,height//2+80,150,50)
+
+#Draw astart button
+start_button=pygame.Rect(width//2-75,height//2+30,150,55)
+
+title_font=pygame.font.Font(None,70)
+menu_font=pygame.font.Font(None,32)
+
+background=pygame.image.load("images/background.png").convert()
+background=pygame.transform.scale(background,(width,height))
 
 # The main game loop
 while running:
@@ -93,12 +103,15 @@ while running:
             running = False
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-
+            if show_menu and start_button.collidepoint(event.pos):
+                show_menu=False
             if game_over and restart_button.collidepoint(event.pos):
 
                 # Reset game
                 game_over = False
                 score = 0
+                speed=3
+                enemy_speed=5
                 crash_sound.stop()
                 pygame.mixer.music.play(-1)
                 player_x = (width-player_width)//2
@@ -106,10 +119,10 @@ while running:
 
                 enemies=create_enemies(8)
 
-
+    
     # ---------------- GAME LOGIC ----------------
 
-    if not game_over:
+    if not game_over and not show_menu:
     
         # Player movement
         keys = pygame.key.get_pressed()
@@ -142,12 +155,8 @@ while running:
                 )
 
                 enemy_pos[1] = random.randint(-1200,-100)
-        if score>=500:
-            speed=5
-            enemy_speed=7
-        else:
-            speed=3
-            enemy_speed=5
+        enemy_speed=5+score//300
+        enemy_speed=min(enemy_speed,10)
         # Player collision rectangle
         player_rect = pygame.Rect(
             player_x + 15,
@@ -178,79 +187,126 @@ while running:
 
     # ---------------- DRAW ----------------
 
-    screen.fill(white)
+    
+    screen.blit(background,(0,0))
+
+    if show_menu:
+        title=title_font.render("SPACE DODGER", True,(0,0,150))
+        title_rect=title.get_rect(center=(width//2,80))
+        screen.blit(title,title_rect)
+
+        text1=menu_font.render("Use Arrow keys to move", True,white)
+        text2=menu_font.render("Avoid Enemy Spaceships",True,white)
+
+        screen.blit(text1,(110,150))
+        screen.blit(text2,(95,190))
+
+        pygame.draw.rect(screen,(0,150,0),start_button)
+        start_text=menu_font.render("START",True, (255,255,255))
+
+        start_rect=start_text.get_rect(center=start_button.center)
+        screen.blit(start_text,start_rect)
+    else:
 
     # Draw player
-    screen.blit(player, (player_x, player_y))
+        screen.blit(player, (player_x, player_y))
 
     # Draw enemies
-    for enemy_pos in enemies:
-        screen.blit(
-            enemy,
-            (enemy_pos[0], enemy_pos[1])
-        )
+        for enemy_pos in enemies:
+            screen.blit(
+                enemy,
+                (enemy_pos[0], enemy_pos[1])
+            )
 
 
     # Draw score
-    score_text = score_font.render(
-        f"Score: {score}",
-        True,
-        (0, 0, 0)
-    )
+        score_text = score_font.render(
+            f"Score: {score}",
+            True,
+            white
+        )
 
-    screen.blit(score_text, (10, 10))
+        screen.blit(score_text, (10, 10))
 
     #Draw highscore
-    highscore_text = score_font.render(
+        highscore_text = score_font.render(
             f"High Score: {highscore}",
             True,
-            (0, 0, 0)
+            white
         )
     
-    screen.blit(highscore_text, (300, 10))
+        screen.blit(highscore_text, (300, 10))
     
 
     # ---------------- GAME OVER ----------------
 
-    if game_over:
+        if game_over:
 
-        game_over_text = font.render(
-            "GAME OVER",
-            True,
-            (255, 0, 0)
-        )
+            # adding dark overlay 
+            # Create a semi-transparent black overlay
+            # This darkens the screen so the Game Over text is easier to read
+            overlay=pygame.Surface((width,height))
+            overlay.set_alpha(170)  #   transparency
+            overlay.fill((0,0,0))
+            screen.blit(overlay,(0,0))
 
-        text_rect = game_over_text.get_rect(
-            center=(width // 2, height // 2 - 30)
-        )
+            #create game over text
+            game_over_text = font.render(
+                "GAME OVER",
+                True,
+                (255, 0, 0)
+            )
 
-        screen.blit(
-            game_over_text,
-            text_rect
-        )
+            #position the text near the top center of the screen 
+            text_rect = game_over_text.get_rect(
+                center=(width // 2, height // 2 - 70)
+            )
 
+            #display the game over text
+            screen.blit(
+                game_over_text,
+                text_rect
+            )
+
+            final_score=restart_font.render(f"Final Score:{score}",True,white)
+
+            final_rect=final_score.get_rect(center=(width//2,height//2))
+            screen.blit(final_score,final_rect)
+
+            best_score = restart_font.render(
+                f"High Score: {highscore}",
+                True,
+                white
+            )
+
+            best_rect = best_score.get_rect(
+                center=(width//2, height//2+30)
+            )
+
+            screen.blit(best_score, best_rect)
 
         # Draw restart button
-        pygame.draw.rect(
-            screen,
-            (0, 0, 0),
-            restart_button
-        )
+            pygame.draw.rect(
+                screen,
+                (40, 40, 40),
+                restart_button,
+                border_radius=12
+            )
+            pygame.draw.rect(screen,white,restart_button,2,border_radius=12)
+            restart_text = restart_font.render(
+                "RESTART",
+                True,
+                (255, 255, 255)
+            )
 
-        restart_text = restart_font.render(
-            "RESTART",
-            True,
-            (255, 255, 255)
-        )
+            restart_text_rect = restart_text.get_rect(
+                center=restart_button.center
+            )
 
-        restart_text_rect = restart_text.get_rect(
-            center=restart_button.center
-        )
-
-        screen.blit(
-            restart_text,
-            restart_text_rect
-        )
+            screen.blit(
+                restart_text,
+                restart_text_rect
+            )
 
 
     # Update display
